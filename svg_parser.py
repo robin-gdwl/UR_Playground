9a2da364e6921f06d63ca79cea481b2edcbf520c
Robin Godwyll <rodwyll+github@gmail.com>
2019-11-17 03:34:19 +0100

    svg gradient parsing

--- svg_parser.py
+++ svg_parser.py
@@ -1,13 +1,14 @@
 import svgpathtools
 import numpy as np
 import matplotlib.pyplot as plt
+from xml.dom import minidom
 
 class SVGParse:
 
     def __init__(self, filepath, tolerance):
         self.path = filepath
         self.tol = tolerance
-
+        self.gradients = self.get_gradients()
 
 
     # make this a staticmethod ??
@@ -15,11 +16,6 @@ class SVGParse:
 
         ob_list = []
         paths, attributes = svgpathtools.svg2paths(self.path)
-
-        #print("paths: ", paths)
-        #print(attributes)
-        # print(file_att)
-
 
         i = 0
         for p in paths:
@@ -40,12 +36,13 @@ class SVGParse:
                 print("no opacity")
 
             stroke = p_attributes["stroke"]
+
             if "url" in stroke:
                 print("gradient in p")
                 gradient_id = stroke[stroke.find("(") + 1:stroke.find(")")]
                 gradient_id = gradient_id.strip("#")
                 print(gradient_id)
-                gradient = get_gradient(gradient_id)
+                gradient = self.get_gradients()
 
             else:
                 print("no gradient")
@@ -79,20 +76,64 @@ class SVGParse:
 
         #return list
 
-
     def get_color(self, param, colour):
         pass
 
     def get_opacity(self, param, opacity):
         pass
 
-    def get_gradient(self, url):
-        xml_file = minidom.parse(filepath)
+    def get_gradients(self):
+        xml_file = minidom.parse(self.path)
         gradients = xml_file.getElementsByTagName("linearGradient")
+
+
         print(gradients)
 
+        for g in gradients:
+            print("g:", g)
+            id = g.attributes["id"].value
+            #g_xml = g.toxml()
+
+            stops = g.getElementsByTagName("stop")
+            for stop in stops:
+                offset = stop.attributes["offset"].value
+                style = stop.attributes["style"].value
+
+
+
+                col_desc = "stop-color:"
+                if style.find(col_desc) != -1:
+                    color_ind = style.index(col_desc) + len(col_desc)
+                    color_val = style[color_ind: color_ind + 7]
+                else:
+                    color_val = None
+                print(color_val)
+
+                opa_desc = "stop-opacity:"
+                if style.find(opa_desc) != -1:
+                    opa_ind = style.index(opa_desc) + len(opa_desc)
+                    opa_val = style[opa_ind: opa_ind + 7]
+                else:
+                    opa_val = 1
+                print(opa_val)
+
+                print(offset , style)
+            print("s: ", stops)
+
+
+            #stop = g.childNodes
+            print(g.attributes)
+            #print(g.toxml())
+
+
+
+
+        return gradients
+
+
 testfile = "gradient05.svg"
-coords = SVGParse(testfile,20).convert_to_coords()
+coords = SVGParse(testfile,20)
+coords = coords.convert_to_coords()
 
 print(coords)
 x = []
